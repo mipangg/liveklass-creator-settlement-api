4b0168d4bb8f779f65c6ad274be57b7d1e00aa36
mipangg <morganjeong119@gmail.com>
2026-04-29 14:02:34 +0900

    chore: update pr template

---
+++ src/test/java/io/mipangg/liveklasscreatorsettlementapi/LiveklassCreatorSettlementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.mipangg.liveklasscreatorsettlementapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveklassCreatorSettlementApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
